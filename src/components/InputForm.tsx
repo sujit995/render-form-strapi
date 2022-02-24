59b20adf24a9843e3f0b5a746313bddc1c07222b
sujit995 <sujitmishra402@gmail.com>
2022-02-24 15:52:26 +0530

    create input form with alidation

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -1,10 +1,69 @@
 import React from 'react'
+import { FieldValues, UseFormRegister } from 'react-hook-form';
+import '../styles/inputform.css';
 
-type Props = {}
+interface Props{
+  type:string;
+  label:string;
+  style?:string;
+  placeholder?:string;
+  register?:UseFormRegister<FieldValues>;
+  error?:{[key: string]: any};
+  
+}
 
-function InputForm({}: Props) {
+const checkRequired =(label:string):boolean=>{
+  switch(label){
+      case 'Full Name':
+      case 'Resume/CV':
+      case 'Email':
+      return true;
+  }
+  return false;
+}
+
+const getValidation = (label:string)=>{
+  var obj: {[key: string]: any} = {};
+  switch(label){
+      case 'Full Name':
+          obj.required = true;
+          obj.minLength = 10;
+          obj.pattern = /[a-zA-Z][a-zA-Z ]+/
+          return obj;
+      case 'Email':
+          obj.required = true;
+          obj.pattern=/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+          return obj;
+      case 'Phone':
+          obj.pattern=/(\+[\d]{1,5}|0)[7-9]\d{9}$/
+          obj.required=true
+          return obj;
+      case 'LinkedIn URL' :
+          obj.pattern=/((https?:\/\/)?((www|\w\w)\.)?linkedin\.com\/)((([\w]{2,3})?)|([^\/]+\/(([\w|\d-&#?=])+\/?){1,}))$/
+          return obj;
+          
+      default : return obj;
+  }
+}
+
+const toPascalCase = (sentence:string) => sentence
+.split(' ')
+.map(word => word[0]
+.toUpperCase()
+.concat(word.slice(1)))
+.join('');
+
+export const InputForm:React.FC<Props> = ({type,label,placeholder,register,error}):JSX.Element => {
   return (
-    <div>InputForm</div>
+    <>
+      <div className="row d-flex mt-4 m-3">
+      <label className="col-lg-3">{label}{checkRequired(label)?<span className="star">✱</span>:null}</label>
+      <input className="col-lg-6" id="formInput" { ...register?{...register(toPascalCase(label),getValidation(label))}:null } type={type} placeholder={placeholder} />
+            {error?error[toPascalCase(label)]?.type === 'required' && <p style={{color:'red', minWidth:'300px'}}>This Field is required</p>:null}
+            {error?error[toPascalCase(label)]?.type === 'pattern' && <p style={{color:'red', minWidth:'300px'}}>{label==='Phone'?`Invalid Phone number`:`Invalid ${label}`}</p>:null}
+            {error?error[toPascalCase(label)]?.type === 'minLength' && <p style={{color:'red', minWidth:'300px'}}>Value cant be less than 10 characters</p>:null}
+      </div>
+    </>
   )
 }
 
